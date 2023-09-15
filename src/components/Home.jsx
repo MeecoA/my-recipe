c86755f15221fe28267c31828437185dfb7ffc17
meecoa <meecorcll@gmail.com>
2023-09-15 12:56:14 +0800

    added home random recipe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,32 @@
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
+import GradeIcon from "@mui/icons-material/Grade";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import Tooltip from "@mui/material/Tooltip";
 
 const Home = () => {
+
+  const randomurl = "https://www.themealdb.com/api/json/v1/1/random.php";
+  const [meal, setMeal] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: randomurl,
+    }).then((response) => {
+      console.log(response.data);
+      setMeal(response.data.meals[0]);
+    });
+  }, []);
+
   return (
     <div className="typewriter">
       <div>
@@ -19,6 +45,30 @@ const Home = () => {
           <span style={{ color: "blue" }}> Globe</span>.
         </Tooltip>
       </h2>
+      <div></div>
+      <div><h3>TRY IT OUT!</h3></div>
+      <div>
+      
+      <div className="meal-card" key={meal.idMeal}>
+                  <div className="meal-img">
+                    <img id="mealImg" src={meal.strMealThumb} alt="" />
+                  </div>
+                  <div id="mealName" className="mealNameRecipe">
+                  {meal.strMeal}
+                  </div>
+                  <div className="btn-container-recipe">
+                    <Tooltip title="Go to Recipe" placement="right">
+                      <Link to={`/recipe-details/${meal.idMeal}`}>
+                        <Button variant="outlined">
+                          <div className="go-to-link">
+                            <RestaurantMenuIcon />
+                          </div>
+                        </Button>
+                      </Link>
+                    </Tooltip>
+                  </div>
+                </div>
+      </div>
     </div>
   );
 };
